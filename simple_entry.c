1cf94404f57481eda79d515765f66fd25b14fd7c
Ashish Tyagi <2ashishtyagi@gmail.com>
2018-04-16 14:10:49 -0400

    Replaced tabs with 2 spaces

--- simple_entry.c
+++ simple_entry.c
@@ -24,50 +24,50 @@ static long drop_all = 0;
 
 
 static unsigned int hook_func(
-	void *priv,
-	struct sk_buff *skb,
-	const struct nf_hook_state *state
+  void *priv,
+  struct sk_buff *skb,
+  const struct nf_hook_state *state
 ) {
-	struct iphdr *ip_header;
-	struct icmphdr *icmp_header;
-	if (!skb)
-		return NF_ACCEPT;
-	ip_header = (struct iphdr *)skb_network_header(skb);
-	if (ip_header->protocol == IPPROTO_ICMP) {
-		icmp_header = (struct icmphdr *)(skb_transport_header(skb) +
-				ip_hdrlen(skb));
-			pr_info("SRC: (%pI4) --> DST: (%pI4) ICMP type: %d - ICMP code: %d\n",
-					&ip_header->saddr,
-					&ip_header->daddr,
-					icmp_header ? icmp_header->type : 0,
+  struct iphdr *ip_header;
+  struct icmphdr *icmp_header;
+  if (!skb)
+    return NF_ACCEPT;
+  ip_header = (struct iphdr *)skb_network_header(skb);
+  if (ip_header->protocol == IPPROTO_ICMP) {
+    icmp_header = (struct icmphdr *)(skb_transport_header(skb) +
+        ip_hdrlen(skb));
+    pr_info("SRC: (%pI4) --> DST: (%pI4) ICMP type: %d - ICMP code: %d\n",
+          &ip_header->saddr,
+          &ip_header->daddr,
+          icmp_header ? icmp_header->type : 0,
           icmp_header ? icmp_header->code : 0
-			);
-	}
-	return drop_all == 0 ? NF_ACCEPT : NF_DROP;
+    );
+  }
+  return drop_all == 0 ? NF_ACCEPT : NF_DROP;
 }
 
 static int __init mod_init(void)
 {
-	int retcode = 0;
-	hook_data.hook = hook_func;
-	hook_data.hooknum = NF_INET_PRE_ROUTING;
-	hook_data.pf = PF_INET;
-	hook_data.priority = NF_IP_PRI_LAST;
-	retcode = nf_register_net_hook(&init_net, &hook_data);
-	if (retcode) {
-		pr_err("Failed to register NetFilter hook\n");
-		return retcode;
-	}
+  int retcode = 0;
+  hook_data.hook = hook_func;
+  hook_data.hooknum = NF_INET_PRE_ROUTING;
+  hook_data.pf = PF_INET;
+  hook_data.priority = NF_IP_PRI_LAST;
+  retcode = nf_register_net_hook(&init_net, &hook_data);
+  if (retcode) {
+    pr_err("Failed to register NetFilter hook\n");
+    return retcode;
+  }
 
-	pr_info("Registered NetFilter Module\n");
+  pr_info("Registered NetFilter Module\n");
 
-	return retcode;
+  return retcode;
 }
 
 static void __exit mod_exit(void)
 {
-	nf_unregister_net_hook(&init_net, &hook_data);
-	pr_info("Unregistered NetFilter Module\n");
+  nf_unregister_net_hook(&init_net, &hook_data);
+  pr_info("Unregistered NetFilter Module\n");
 }
 
 module_init(mod_init);
